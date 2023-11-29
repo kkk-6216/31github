a6e74e07993254731a96c28de4b7401dc0da3efe
kkk-6216 <ekadyrkulov6216@gamil.com>
2023-11-29 15:35:37 +0600

    second controller: CategoryController

--- shoppers/src/main/java/com/pro/shoppers/client/controller/CategoryController.java
+++ shoppers/src/main/java/com/pro/shoppers/client/controller/CategoryController.java
@@ -3,10 +3,9 @@ package com.pro.shoppers.client.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ProductController {
+public class CategoryController {
 
-    public ProductController() {
-
+    public CategoryController() {
+        
     }
-
 }
