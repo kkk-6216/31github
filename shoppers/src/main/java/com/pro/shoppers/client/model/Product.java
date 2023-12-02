0a43dd2c462151e0e6a120b0182be84901450e43
kkk-6216 <98480774+kkk-6216@users.noreply.github.com>
2023-12-02 18:35:31 +0600

    Product entity

--- shoppers/src/main/java/com/pro/shoppers/client/model/Product.java
+++ shoppers/src/main/java/com/pro/shoppers/client/model/Product.java
@@ -1,3 +1,29 @@
+package com.pro.shoppers.client.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "description")
+    private String description;
 
 }
