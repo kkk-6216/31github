9b655a5c792d995f65e0dbc84fc3d81f7951ac5b
kkk-6216 <98480774+kkk-6216@users.noreply.github.com>
2023-12-24 00:02:46 +0600

    Update 1496. Path Crossing.java

---
+++ 1496. Path Crossing.java
@@ -0,0 +1,24 @@
+class Solution {
+    // TC - O(n), n = length of path
+    // SC - O(n)
+    public boolean isPathCrossing(String path) {
+        int x = 0;
+        int y = 0;
+        Set<String> set = new HashSet();
+        String key = x + "$" + y;
+        set.add(key);
+        
+        for(char c : path.toCharArray()) {
+            if(c == 'N') y++;
+            else if(c == 'S') y--;
+            else if(c == 'E') x++;
+            else x--;
+            key = x  + "$" + y;
+            if(set.contains(key))
+                return true;
+            set.add(key);
+        }
+        
+        return false;
+    }
+}
